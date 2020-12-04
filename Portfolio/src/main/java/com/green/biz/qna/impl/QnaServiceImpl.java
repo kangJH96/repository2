23f1b8800546c6053eccedd27ab84b05ec833f37
kjh <kjunho0502@naver.com>
2020-12-04 23:03:57 +0900

    2020-12-04(금) masterBranch

--- Portfolio/src/main/java/com/green/biz/qna/impl/QnaServiceImpl.java
+++ Portfolio/src/main/java/com/green/biz/qna/impl/QnaServiceImpl.java
@@ -3,7 +3,7 @@ package com.green.biz.qna.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Service;   
 
 import com.green.biz.qna.QnaService;
 import com.green.biz.qna.QnaVO;
